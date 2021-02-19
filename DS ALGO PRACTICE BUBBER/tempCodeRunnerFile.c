29e7958eaec56c588f046c589a1da1ec322a9596
ritikkadole <77978137+ritikkadole@users.noreply.github.com>
2021-02-19 09:47:40 +0530

    commiting the mid time table

--- DS ALGO PRACTICE BUBBER/tempCodeRunnerFile.c
+++ DS ALGO PRACTICE BUBBER/tempCodeRunnerFile.c
@@ -1,9 +1,44 @@
-for (int i = 0; i < l; i++)
+#include <stdio.h>
+#include <conio.h>
+#include<time.h>
 
-//     {
-//         printf("%d", i);
+void delay(int number_of_seconds) 
+{ 
+    // Converting time into milli_seconds 
+    int milli_seconds = 1000 * number_of_seconds; 
+  
+    // Storing start time 
+    clock_t start_time = clock(); 
+  
+    // looping till required time is not achieved 
+    while (clock() < start_time + milli_seconds) 
+        ; 
+} 
 
-//         printf("here is string %d = ", i + 1);
-//         puts(arr[i]);
-//         printf("\n");
-//     }
+
+void main()
+{
+
+int n;
+enteragain:
+printf("Enter a number greater than 77 = ");
+scanf("%d",&n);
+if (n>77)
+{}
+else 
+{
+    goto enteragain; 
+}
+
+for(int i=0;i<n;i++)
+{
+
+printf("%c\n",7);
+
+delay(1); 
+
+}
+
+getch();
+
+}
