53691fec7a6d008392326289a3593ec87b999a7f
ritikkadole <77978137+ritikkadole@users.noreply.github.com>
2021-03-13 22:30:47 +0530

    here

--- CPP Programs/faltu.cpp
+++ CPP Programs/faltu.cpp
@@ -1,22 +1,64 @@
 #include <iostream>
 #include <string>
 #include <conio.h>
+#include<string.h>
+
 
 using namespace std;
 
 class student
 {
+
+
+    // this all below are the member of the class / object student in private protected and public
 public:
     int Roll = 0;
  string name;
+// this below is the example of the constructor
+
+// student(string n, int r )
+// {
 
 
+// this->name=n;
+// this->Roll=r;
+// //this->pass=pas;
+
+
+
+// }
+
+
+
+// this below is the wxample of class method 
+
+void get()
+{
+
+cout<<"Enter the name ";   // GETTING NAME
+// cin>>this->name;
+
+gets(this->name);
+
+cout<<"Enter the name "; // GETTING ROLL
+cin>>this->Roll;
+
+cout<<endl<<endl;   // GETTING NAME
+
+// cout<<"Enter the pass ";   // GETIIING PASS
+// cin>>this->pass;
+
+
+
+
+}
 
  void pri()
    {
 
-    cout << this->name <<endl<<this->Roll;
+    cout <<"NAME = "<< this->name <<endl<< "ROLL = "<<this->Roll;
 
+cout<<endl<<endl;   // GETTING NAME
 
    }
 
@@ -35,20 +77,28 @@ int main()
 
 {
 
-    student s1;
-
-    s1.Roll = 85;
-    s1.name = "ritik";
-
-cout<<"enter the Name of s1"<<endl;
-cin>>s1.name;
-
-cout<<"enter the Roll of s1"<<endl;
-cin>>s1.Roll;
+    student s1[5];
 
 
+//     s1.Roll = 85;
+//     s1.name = "ritik";
 
-    s1.pri();
+// cout<<"enter the Name of s1"<<endl;
+// cin>>s1.name;
+
+// cout<<"enter the Roll of s1"<<endl;
+// cin>>s1.Roll;
+
+for(int i=0;i<5;i++)
+{
+ 
+    s1[i].get();
+    s1[i].pri();
+
+
+
+}
+
 
 
    // cout<<sizeof(string);
